3e148d1323d55f726e4df3b88d84fc20d3819e12
Jocelyn Liu <jocelynliu2000@gmail.com>
2022-03-09 03:00:30 -0800

    completed login and register page layouts, implemented upload profile image function on registration page

--- app/src/main/java/com/example/csci310/CreateAccountActivity.java
+++ app/src/main/java/com/example/csci310/CreateAccountActivity.java
@@ -1,16 +1,70 @@
 package com.example.csci310;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateAccountActivity extends AppCompatActivity {
+    Button uploadButton;
+    ImageView previewImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ImageView imageView = (ImageView) findViewById(R.id.preview_image);
+//        imageView.setImageResource(R.drawable.);
         setContentView(R.layout.activity_createacct);
 
+        uploadButton = (Button) findViewById(R.id.upload_button);
+        previewImage = (ImageView) findViewById(R.id.preview_image);
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { uploadPicture(); }
+        });
     }
+
+    public void uploadPicture() {
+        activityLauncher.launch("image/*");
+    }
+
+    ActivityResultLauncher<String> activityLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    previewImage.setImageURI(uri);
+                }
+            });
+
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (resultCode == RESULT_OK) {
+//
+//            // compare the resultCode with the
+//            // SELECT_PICTURE constant
+//            if (requestCode == SELECT_PICTURE) {
+//                // Get the url of the image from data
+//                Uri selectedImageUri = data.getData();
+//                if (null != selectedImageUri) {
+//                    // update the preview image in the layout
+//                    previewImage.setImageURI(selectedImageUri);
+//                }
+//            }
+//        }
+//    }
+
+
 }
