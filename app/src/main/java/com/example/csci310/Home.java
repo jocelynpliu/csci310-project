a1600c51127470bb9d932237fc68d9afd7caebdd
Justin Nakama <jsnakama@gmail.com>
2022-03-17 15:50:49 -0700

    added recylcer + adapter to view public events at home page

--- app/src/main/java/com/example/csci310/Home.java
+++ app/src/main/java/com/example/csci310/Home.java
@@ -1,12 +1,17 @@
 package com.example.csci310;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
 
 public class Home extends AppCompatActivity {
     private Button homeButton;
+    String s1[];
+
+    RecyclerView homeRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,14 @@ public class Home extends AppCompatActivity {
 
         homeButton = (Button) findViewById(R.id.homeButton);
         homeButton.setAlpha(.5f);
+
+        s1 = getResources().getStringArray(R.array.testEvents);
+
+       homeRecyclerView = findViewById(R.id.homeRecyclerView);
+
+        HomeRecyclerAdapter myAdapter = new HomeRecyclerAdapter (this, s1);
+        homeRecyclerView.setAdapter(myAdapter);
+        homeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
---
+++ app/src/main/java/com/example/csci310/HomeRecyclerAdapter.java
@@ -0,0 +1,61 @@
+package com.example.csci310;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class HomeRecyclerAdapter extends RecyclerView.Adapter<HomeRecyclerAdapter.MyViewHolder> {
+
+    private ArrayList<Event> publicEventList;
+    String data1[];
+    Context context;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView myTextView;
+
+        public MyViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+            myTextView = (TextView) view.findViewById(R.id.homeEvent);
+        }
+
+        public TextView getTextView() {
+            return myTextView;
+        }
+    }
+
+
+
+    public HomeRecyclerAdapter(Context ct, String[] s1) {
+
+        data1  = s1;
+        context = ct;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.home_row, parent, false);
+
+        return new MyViewHolder(view) ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeRecyclerAdapter.MyViewHolder holder, int position) {
+        holder.myTextView.setText(data1[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data1.length;
+    }
+}
