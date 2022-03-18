35e6e9ef0435a30f08e24d28a0bc0d7d9b36b9b8
Justin Nakama <jsnakama@gmail.com>
2022-03-18 00:12:54 -0700

    can't figure out how to make items in list clickable, pushing work so far

--- app/src/main/java/com/example/csci310/Home.java
+++ app/src/main/java/com/example/csci310/Home.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 
 public class Home extends AppCompatActivity {
@@ -18,21 +21,38 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Log.d("----------start", " Home.java");
+
+        //gray out the button to make it look selected and cool
         homeButton = (Button) findViewById(R.id.homeButton);
         homeButton.setAlpha(.5f);
 
         //just for testing purposes
-//        s1 = getResources().getStringArray(R.array.testEvents);
+        //later, get all event objects titles in a list, sorted order
         s1 = new String[]{"event 1", "event 2", "event3", "event4",
                 "event 5", "event 6", "event 7", "event 8", "event 9",
         "event 10", "event 11"};
 
+        //recycler and adapter needed to display a dynamic list on the screen
        homeRecyclerView = findViewById(R.id.homeRecyclerView);
        homeRecyclerView.setNestedScrollingEnabled(false);
 
+        homeRecyclerView.setHasFixedSize(true);
+
+        //specific array of data goes into this adapter
         HomeRecyclerAdapter myAdapter = new HomeRecyclerAdapter (this, s1);
+
+
         homeRecyclerView.setAdapter(myAdapter);
         homeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        myAdapter.setOnItemClickListener(new HomeRecyclerAdapter.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(int position) {
+                Log.d(String.valueOf(position), "Clicked!!");
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/csci310/HomeRecyclerAdapter.java
+++ app/src/main/java/com/example/csci310/HomeRecyclerAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,38 +19,63 @@ public class HomeRecyclerAdapter extends RecyclerView.Adapter<HomeRecyclerAdapte
     String data1[];
     Context context;
 
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener {
+        public void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener = listener;
+    }
+
+    //view holder object goes into adapter object in Home.java
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         TextView myTextView;
 
-        public MyViewHolder(View view) {
+        public MyViewHolder(View view, final OnItemClickListener listener) {
             super(view);
             // Define click listener for the ViewHolder's View
             myTextView = (TextView) view.findViewById(R.id.homeEvent);
-        }
 
-        public TextView getTextView() {
-            return myTextView;
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
         }
     }
 
 
 
-    public HomeRecyclerAdapter(Context ct, String[] s1) {
 
+
+// adapter made to only take in one string of array, may need to edit later
+    public HomeRecyclerAdapter(Context ct, String[] s1) {
         data1  = s1;
         context = ct;
-
     }
 
+    //utilizes home_row.xml object, make a bunch and fills them with passed in data
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.home_row, parent, false);
 
-        return new MyViewHolder(view) ;
+        MyViewHolder evh = new MyViewHolder(view, mListener);
+
+        return evh;
     }
 
+    //these two functions just necessary for adaptor to work, small edits for passed
+    //in data type later maybe
     @Override
     public void onBindViewHolder(@NonNull HomeRecyclerAdapter.MyViewHolder holder, int position) {
         holder.myTextView.setText(data1[position]);
