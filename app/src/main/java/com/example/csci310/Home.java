4c01a095ac604851f7edb9681780f869ae17baf2
Justin Nakama <jsnakama@gmail.com>
2022-03-17 18:28:18 -0700

    all scrollable events in home page are visible now

--- app/src/main/java/com/example/csci310/Home.java
+++ app/src/main/java/com/example/csci310/Home.java
@@ -21,9 +21,14 @@ public class Home extends AppCompatActivity {
         homeButton = (Button) findViewById(R.id.homeButton);
         homeButton.setAlpha(.5f);
 
-        s1 = getResources().getStringArray(R.array.testEvents);
+        //just for testing purposes
+//        s1 = getResources().getStringArray(R.array.testEvents);
+        s1 = new String[]{"event 1", "event 2", "event3", "event4",
+                "event 5", "event 6", "event 7", "event 8", "event 9",
+        "event 10", "event 11"};
 
        homeRecyclerView = findViewById(R.id.homeRecyclerView);
+       homeRecyclerView.setNestedScrollingEnabled(false);
 
         HomeRecyclerAdapter myAdapter = new HomeRecyclerAdapter (this, s1);
         homeRecyclerView.setAdapter(myAdapter);
